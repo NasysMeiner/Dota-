fb611069304d78dc8a59ec9f7a627a0d6864f249
NasysMeiner <iguana10053@gmail.com>
2024-02-20 08:10:14 +0300

    Bullet_Fix

--- Assets/Data/Script/Towers/Bullet.cs
+++ Assets/Data/Script/Towers/Bullet.cs
@@ -1,49 +1,42 @@
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private float _damage;
 
+    private float _damage;
     private IEntity _target;
-    private Vector3 _targetPosition;
+    private Rigidbody2D _rigidbody;
 
     public void Initialization(IEntity target, float damage)
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
+
         _damage = damage;
         _target = target;
-        _targetPosition = _target.Position;
+
+        PullOutOfGun();
     }
 
     private void Update()
     {
-        if (_target != null)
-        {
-            if (Vector3.Distance(transform.position, _targetPosition) > 6f) 
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                _targetPosition = _target.Position;
-            }
-        }
-        if (Vector3.Distance(transform.position, _targetPosition) < 0.05f)
-        {
-            _target.GetDamage(_damage);
+        if (Mathf.Abs((_target.Position - transform.position).magnitude) < 0.2f)
             Destroy(gameObject);
-        }
-        transform.position = Vector3.MoveTowards(transform.position, _targetPosition, _speed * Time.deltaTime);
     }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.TryGetComponent(out IEntity enemy) && enemy == _target)
-    //    {
-    //        Debug.Log(enemy + " " + _damage);
-    //        enemy.GetDamage(_damage);
-    //        Destroy(gameObject);
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IEntity enemy) && enemy == _target)
+        {
+            enemy.GetDamage(_damage);
+            Destroy(gameObject);
+        }
+    }
+
+    public void PullOutOfGun()
+    {
+        _rigidbody.velocity = (_target.Position - transform.position).normalized * _speed;
+    }
 }
