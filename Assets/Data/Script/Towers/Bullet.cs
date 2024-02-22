4e101ba46edecf9672801ca86da1f543b8feed83
NasysMeiner <iguana10053@gmail.com>
2024-02-22 11:48:45 +0300

    Effect

--- Assets/Data/Script/Towers/Bullet.cs
+++ Assets/Data/Script/Towers/Bullet.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -6,7 +7,18 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
 
+    [Header("Effects")]
+    [SerializeField] private Effect _startEffect;
+    [SerializeField] private float _timeStartEffect;
+
+    [SerializeField] private Effect _updateEffect;
+
+    [SerializeField] private Effect _endEffect;
+    [SerializeField] private float _timeEndEffect;
+
     private float _damage;
+    private bool _isDestroy = false;
+    private float _timeDestroy;
     private Vector3 _target;
     private Rigidbody2D _rigidbody;
 
@@ -23,26 +35,54 @@ public class Bullet : MonoBehaviour
     private void Update()
     {
         if (Mathf.Abs((_target - transform.position).magnitude) <= 0.05f)
-            Destroy(gameObject);
+            Destroy();
+
+        if (_isDestroy)
+        {
+            _timeDestroy += Time.deltaTime;
+
+            if (_timeDestroy >= _timeEndEffect + 0.5f)
+                Destroy(gameObject);
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (_updateEffect != null)
+            _updateEffect.StopEffect();
+
         IEntity enemy = collision.GetComponent<IEntity>();
+
         if (enemy != null)
-        {
             enemy.GetDamage(_damage);
-        }
-        Destroy(gameObject);
-        //if (collision.TryGetComponent(out IEntity enemy))
-        //{
-        //    enemy.GetDamage(_damage);
-        //    Destroy(gameObject);
-        //}
+
+        Destroy();
     }
 
     public void PullOutOfGun()
     {
         _rigidbody.velocity = (_target - transform.position).normalized * _speed;
+
+        if (_updateEffect != null)
+            _updateEffect.StartEffect();
+    }
+
+    private void Destroy()
+    {
+        _isDestroy = true;
+        _rigidbody.velocity = Vector3.zero;
+
+        if(_endEffect != null)
+            StartCoroutine(WaitEffect(_endEffect, _timeEndEffect));
+    }
+
+    private IEnumerator WaitEffect(Effect effect, float time)
+    {
+        effect.StartEffect();
+
+        yield return new WaitForSeconds(time);
+
+        effect.StopEffect();
     }
 }
