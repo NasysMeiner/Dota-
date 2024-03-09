9c1f9380c04fd10ef0e470d725e326b020527d19
NasysMeiner <iguana10053@gmail.com>
2024-03-09 17:28:02 +0300

    Sprite Damage

---
+++ Assets/Data/Script/Effects/DamageColorEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DamageColorEffect : MonoBehaviour
+{
+    [SerializeField] private Unit _unit;
+    [Space]
+    [SerializeField] private Color DamageColor = Color.red;
+    [SerializeField] private float DamageTimeSec = 1f;
+
+    private SpriteRenderer _spriteRend;
+    private Color _defaultColor;
+
+    private void OnDisable()
+    {
+        _unit.HealthChanged -= StartEffect;
+    }
+
+    private void Start()
+    {
+        _unit.HealthChanged += StartEffect;
+        _spriteRend = GetComponent<SpriteRenderer>();
+        _defaultColor = _spriteRend.color;
+    }
+
+    public void StartEffect(float health)
+    {
+        StartCoroutine(StartEffectCoroutine());
+    }
+
+    private IEnumerator StartEffectCoroutine()
+    {
+        float time = 0;
+        float step = 1f / DamageTimeSec;
+
+        while (time < DamageTimeSec)
+        {
+            time += Time.deltaTime;
+            _spriteRend.color = Color.Lerp(DamageColor, _defaultColor, step * time);
+
+            yield return null;
+        }
+    }
+}
