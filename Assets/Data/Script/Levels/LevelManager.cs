98d08339a5eb79ffd678ae8f22f62b0bebe48bab
NasysMeiner <iguana10053@gmail.com>
2024-05-11 16:39:57 +0300

    add save

--- Assets/Data/Script/Levels/LevelManager.cs
+++ Assets/Data/Script/Levels/LevelManager.cs
@@ -1,16 +1,23 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    [SerializeField] private bool _isLoad;
+    [Space]
     [SerializeField] private SceneLoader _sceneLoader;
     [SerializeField] private AudioManager _audioManager;
     [SerializeField] private DataCountLevels _countLevels;
+    [SerializeField] private DataCountLevels _currentData;
     [SerializeField] private LevelSelection _prefab;
     [SerializeField] private GameObject _content;
 
     private List<LevelSelection> _selectedLevels = new();
+
+    private void Awake()
+    {
+        Repository.LoadState();
+    }
 
     private void Start()
     {
@@ -19,14 +26,19 @@ public class LevelManager : MonoBehaviour
 
     public void InitMenuLevel()
     {
-        for(int i = 0; i < _countLevels.Levels.Count; i++)
+        _currentData.LoadData(_countLevels);
+
+        if (Repository.TryGetData(out DataScene dataScene))
+            LoadDataDisk(dataScene);
+
+        for (int i = 0; i < _currentData.Levels.Count; i++)
         {
             LevelSelection newSelection = Instantiate(_prefab, _content.transform);
-            newSelection.SetLevel(_countLevels.Levels[i].Scene, _countLevels.Levels[i].Icon, this);
+            newSelection.SetLevel(_currentData.Levels[i], this);
             _selectedLevels.Add(newSelection);
         }
 
-        LoadJsonData();
+        SaveDataDisk();
     }
 
     public void LoadLevel(string name)
@@ -35,8 +47,45 @@ public class LevelManager : MonoBehaviour
         _sceneLoader.LoadLevel(name);
     }
 
-    private void LoadJsonData()
+    private void LoadDataDisk(DataScene dataScene)
     {
-        //???
+        _currentData.LoadData(dataScene);
+    }
+
+    private void SaveDataDisk()
+    {
+        DataScene dataScene = new();
+        dataScene.InitDataScene(_currentData);
+
+        Repository.SetData(dataScene);
+    }
+}
+
+[System.Serializable]
+public class DataScene
+{
+    public List<SceneCell> Scenes = new();
+
+    public void InitDataScene(DataCountLevels dataCountLevels)
+    {
+        Scenes.Clear();
+
+        foreach(LevelData levelData in dataCountLevels.Levels)
+        {
+            Scenes.Add(new SceneCell(levelData.Scene, levelData.status));
+        }
+    }
+}
+
+[System.Serializable]
+public class SceneCell
+{
+    public string Name;
+    public int Status;
+
+    public SceneCell(string name, int status)
+    {
+        Name = name;
+        Status = status;
     }
 }
